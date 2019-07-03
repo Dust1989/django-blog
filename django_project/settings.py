d96a2a4538612ef18ded4a60195c44ab5524cf2a
Dust-PC\Dust <destiny2013@yeah.net>
2019-07-03 18:08:25 +0800

    finished the base blog

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,3 +124,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.qq.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_HOST_USER = "458568533@qq.com"
+EMAIL_HOST_PASSWORD = "jkyscolzptmmcbdd"
